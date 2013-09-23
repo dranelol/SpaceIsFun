cca84795b77fe9ebc43174ac10738a4648bfc0fb
dranelol <cardsfan853@gmail.com>
2013-09-23 12:35:34 -0500

    segmenting out game states to separate files

--- SpaceIsFun/Game States/Overworld.cs
+++ SpaceIsFun/Game States/Overworld.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+using Ruminate.GUI.Framework;
+using Ruminate.GUI.Content;
 
-namespace SpaceIsFun.Game_States
+namespace SpaceIsFun
 {
-    class Overworld : State
+    public partial class Game1 : Game
     {
     }
 }
