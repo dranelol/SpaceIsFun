0521777cbbcce5157541ad2738d8c6334bc83731
dranelol <cardsfan853@gmail.com>
2013-09-27 10:45:35 -0500

    adding crew object

--- SpaceIsFun/Ship.cs
+++ SpaceIsFun/Ship.cs
@@ -474,6 +474,19 @@ namespace SpaceIsFun
 
     }
 
+    class Crew : Entity
+    {
+        #region fields
+        #endregion
+
+        #region constructors / destructors
+        #endregion
+
+        #region methods
+        #endregion
+
+    }
+
     /// <summary>
     /// Its a ship!
     /// </summary>
